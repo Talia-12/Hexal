907c884abaf68106e83efb40c18c870e30fc0ca7
Talia-12 <paradox.m.burgess@gmail.com>
2022-08-05 17:46:42 +1000

    Rebase testing_entities onto main

--- Common/src/main/java/ram/talia/hexal/common/entities/HexalEntities.java
+++ Common/src/main/java/ram/talia/hexal/common/entities/HexalEntities.java
@@ -1,4 +1,39 @@
 package ram.talia.hexal.common.entities;
 
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.MobCategory;
+import ram.talia.hexal.api.HexalAPI;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.function.BiConsumer;
+
+import static at.petrak.hexcasting.api.HexAPI.modLoc;
+
 public class HexalEntities {
+	public static void registerEntities (BiConsumer<EntityType<?>, ResourceLocation> r) {
+		for (var e : ENTITIES.entrySet()) {
+			r.accept(e.getValue(), e.getKey());
+		}
+	}
+	
+	private static final Map<ResourceLocation, EntityType<?>> ENTITIES = new LinkedHashMap<>();
+	
+	public static final EntityType<TestHurtingProjectile> TEST_HURTING_PROJECTILE = register(
+			"test_hurting_projectile",
+			EntityType.Builder.<TestHurtingProjectile>of(TestHurtingProjectile::new,
+														 MobCategory.MISC).sized(0.1f, 0.1f)
+							  .clientTrackingRange(10)
+							  .updateInterval(1)
+							  .build(HexalAPI.MOD_ID + ":test_hurting_projectile"));
+	
+	private static <T extends Entity> EntityType<T> register (String id, EntityType<T> type) {
+		var old = ENTITIES.put(modLoc(id), type);
+		if (old != null) {
+			throw new IllegalArgumentException("Typo? Duplicate id " + id);
+		}
+		return type;
+	}
 }
--- Fabric/src/main/java/ram/talia/hexal/fabric/FabricHexalInitializer.kt
+++ Fabric/src/main/java/ram/talia/hexal/fabric/FabricHexalInitializer.kt
@@ -2,12 +2,20 @@ package ram.talia.hexal.fabric
 
 import ram.talia.hexal.api.HexalAPI
 import net.fabricmc.api.ModInitializer
+import net.minecraft.core.Registry
 import ram.talia.hexal.common.casting.RegisterPatterns
+import net.minecraft.resources.ResourceLocation
+import ram.talia.hexal.common.entities.HexalEntities
+import java.util.function.BiConsumer
 
 object FabricHexalInitializer : ModInitializer {
     override fun onInitialize() {
         HexalAPI.LOGGER.info("Hello Fabric World!")
 
         RegisterPatterns.registerPatterns()
+        HexalEntities.registerEntities(bind(Registry.ENTITY_TYPE))
     }
+
+    private fun <T> bind(registry: Registry<in T>): BiConsumer<T, ResourceLocation> =
+        BiConsumer<T, ResourceLocation> { t, id -> Registry.register(registry, id, t) }
 }
