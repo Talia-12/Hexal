f8416979670aa0b6928c047f1b83e3854c5903e8
Talia-12 <paradox.m.burgess@gmail.com>
2022-10-01 23:33:52 +1000

    make it so that OpEverbookRead and Write need to target an akashic library, and read/write thier entries from/to that library.

--- Common/src/main/java/ram/talia/hexal/common/casting/actions/everbook/OpEverbookRead.kt
+++ Common/src/main/java/ram/talia/hexal/common/casting/actions/everbook/OpEverbookRead.kt
@@ -1,18 +1,34 @@
 package ram.talia.hexal.common.casting.actions.everbook
 
-import at.petrak.hexcasting.api.spell.ConstManaOperator
-import at.petrak.hexcasting.api.spell.SpellDatum
+import at.petrak.hexcasting.api.spell.*
 import at.petrak.hexcasting.api.spell.casting.CastingContext
-import at.petrak.hexcasting.api.spell.getChecked
 import at.petrak.hexcasting.api.spell.math.HexPattern
+import at.petrak.hexcasting.api.spell.mishaps.MishapNoAkashicRecord
 import at.petrak.hexcasting.api.spell.mishaps.MishapOthersName
+import at.petrak.hexcasting.common.blocks.akashic.BlockEntityAkashicRecord
+import at.petrak.hexcasting.common.lib.HexSounds
+import net.minecraft.core.BlockPos
+import net.minecraft.sounds.SoundSource
+import net.minecraft.world.phys.Vec3
 import ram.talia.hexal.xplat.IXplatAbstractions
 
-object OpEverbookRead : ConstManaOperator {
-	override val argc = 1
+object OpEverbookRead : SpellOperator {
+	override val argc = 2
 
-	override fun execute(args: List<SpellDatum<*>>, ctx: CastingContext): List<SpellDatum<*>> {
-		val key = args.getChecked<HexPattern>(0, OpEverbookDelete.argc)
+	override val isGreat = true
+	override val alwaysProcessGreatSpell = false
+	override val causesBlindDiversion = false
+
+	override fun execute(args: List<SpellDatum<*>>, ctx: CastingContext): Triple<RenderedSpell, Int, List<ParticleSpray>> {
+		val pos = BlockPos(args.getChecked<Vec3>(0, OpEverbookDelete.argc))
+		val key = args.getChecked<HexPattern>(1, OpEverbookDelete.argc)
+
+		ctx.assertVecInRange(Vec3.atCenterOf(pos))
+
+		val tile = ctx.world.getBlockEntity(pos)
+		if (tile !is BlockEntityAkashicRecord) {
+			throw MishapNoAkashicRecord(pos)
+		}
 
 		val iota = IXplatAbstractions.INSTANCE.getEverbookIota(ctx.caster, key)
 
@@ -20,6 +36,18 @@ object OpEverbookRead : ConstManaOperator {
 		if (trueName != null)
 			throw MishapOthersName(trueName)
 
-		return listOf(iota)
+		return Triple(
+			Spell(tile, key, iota),
+			0,
+			listOf()
+		)
+	}
+
+	private data class Spell(val record: BlockEntityAkashicRecord, val key: HexPattern, val datum: SpellDatum<*>) : RenderedSpell {
+		override fun cast(ctx: CastingContext) {
+			record.addNewDatum(key, datum)
+
+			ctx.world.playSound(null, record.blockPos, HexSounds.SCROLL_SCRIBBLE, SoundSource.BLOCKS, 1f, 0.8f)
+		}
 	}
 }
--- Common/src/main/java/ram/talia/hexal/common/casting/actions/everbook/OpEverbookWrite.kt
+++ Common/src/main/java/ram/talia/hexal/common/casting/actions/everbook/OpEverbookWrite.kt
@@ -2,18 +2,32 @@ package ram.talia.hexal.common.casting.actions.everbook
 
 import at.petrak.hexcasting.api.spell.ConstManaOperator
 import at.petrak.hexcasting.api.spell.SpellDatum
+import at.petrak.hexcasting.api.spell.Widget
 import at.petrak.hexcasting.api.spell.casting.CastingContext
 import at.petrak.hexcasting.api.spell.getChecked
 import at.petrak.hexcasting.api.spell.math.HexPattern
+import at.petrak.hexcasting.api.spell.mishaps.MishapNoAkashicRecord
 import at.petrak.hexcasting.api.spell.mishaps.MishapOthersName
+import at.petrak.hexcasting.common.blocks.akashic.BlockEntityAkashicRecord
+import net.minecraft.core.BlockPos
+import net.minecraft.world.phys.Vec3
 import ram.talia.hexal.xplat.IXplatAbstractions
 
 object OpEverbookWrite : ConstManaOperator {
 	override val argc = 2
 
 	override fun execute(args: List<SpellDatum<*>>, ctx: CastingContext): List<SpellDatum<*>> {
-		val key = args.getChecked<HexPattern>(0, argc)
-		val iota = args[1]
+		val pos = BlockPos(args.getChecked<Vec3>(0, argc))
+		val key = args.getChecked<HexPattern>(1, argc)
+
+		ctx.assertVecInRange(Vec3.atCenterOf(pos))
+
+		val tile = ctx.world.getBlockEntity(pos)
+		if (tile !is BlockEntityAkashicRecord) {
+			throw MishapNoAkashicRecord(pos)
+		}
+
+		val iota = tile.lookupPattern(key, ctx.world) ?: SpellDatum.make(Widget.NULL)
 
 		val trueName = MishapOthersName.getTrueNameFromDatum(iota, ctx.caster)
 		if (trueName != null)
