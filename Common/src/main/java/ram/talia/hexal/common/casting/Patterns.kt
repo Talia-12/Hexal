f1cba240d658f862833e0fe6719a110b698c4536
Talia-12 <paradox.m.burgess@gmail.com>
2022-11-21 16:15:52 +1000

    Made OpRunningOp have initial value as a parameter so that RUNNING_MUL doesn't multiply everything by 0.

--- Common/src/main/java/ram/talia/hexal/common/casting/Patterns.kt
+++ Common/src/main/java/ram/talia/hexal/common/casting/Patterns.kt
@@ -49,11 +49,13 @@ object Patterns {
 	@JvmField
 	val FACTORIAL = make(HexPattern.fromAngles("wawdedwaw", HexDir.SOUTH_EAST), modLoc("factorial"), OpFactorial)
 	@JvmField
-	val RUNNING_SUM = make(HexPattern.fromAngles("aea", HexDir.WEST), modLoc("running/sum"), OpRunningOp{ running, iota ->
+	val RUNNING_SUM = make(HexPattern.fromAngles("aea", HexDir.WEST), modLoc("running/sum"), OpRunningOp (0.0)
+	{ running, iota ->
 		running + ((iota as? DoubleIota)?.double ?: throw OpRunningOp.InvalidIotaException("list.double"))
 	})
 	@JvmField
-	val RUNNING_MUL = make(HexPattern.fromAngles("qaawaaq", HexDir.NORTH_EAST), modLoc("running/mul"), OpRunningOp{ running, iota ->
+	val RUNNING_MUL = make(HexPattern.fromAngles("qaawaaq", HexDir.NORTH_EAST), modLoc("running/mul"), OpRunningOp (1.0)
+	{ running, iota ->
 		running * ((iota as? DoubleIota)?.double ?: throw OpRunningOp.InvalidIotaException("list.double"))
 	})
 
--- Common/src/main/java/ram/talia/hexal/common/casting/actions/OpRunningOp.kt
+++ Common/src/main/java/ram/talia/hexal/common/casting/actions/OpRunningOp.kt
@@ -9,12 +9,12 @@ import at.petrak.hexcasting.api.spell.iota.Iota
 import at.petrak.hexcasting.api.spell.mishaps.MishapInvalidIota
 import ram.talia.hexal.api.reductions
 
-class OpRunningOp(private val operator: (Double, Iota) -> Double) : ConstMediaAction {
+class OpRunningOp(private val initial: Double, private val operator: (Double, Iota) -> Double) : ConstMediaAction {
     override val argc = 1
 
     override fun execute(args: List<Iota>, ctx: CastingContext): List<Iota> {
         try {
-            return args.getList(0, argc).reductions(0.0, operator).map { DoubleIota(it) }.toList().asActionResult
+            return args.getList(0, argc).reductions(initial, operator).map { DoubleIota(it) }.toList().asActionResult
         } catch (e: InvalidIotaException) {
             throw MishapInvalidIota.ofType(args[0], 0, e.expectedType)
         }
