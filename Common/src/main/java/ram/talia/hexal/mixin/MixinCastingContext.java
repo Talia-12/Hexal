466ead626e0366d7ca2883121c527cb80a6aedb8
Talia-12 <paradox.m.burgess@gmail.com>
2022-11-22 00:04:11 +1000

    <= instead of <

--- Common/src/main/java/ram/talia/hexal/mixin/MixinCastingContext.java
+++ Common/src/main/java/ram/talia/hexal/mixin/MixinCastingContext.java
@@ -7,13 +7,10 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Slice;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
-import ram.talia.hexal.api.linkable.ILinkable;
 import ram.talia.hexal.api.spell.casting.IMixinCastingContext;
 import ram.talia.hexal.common.entities.BaseCastingWisp;
-import ram.talia.hexal.xplat.IXplatAbstractions;
 
 /**
  * Modifies {@link at.petrak.hexcasting.api.spell.casting.CastingContext} to make it properly allow wisps to affect things within their range.
@@ -52,7 +49,7 @@ public abstract class MixinCastingContext implements IMixinCastingContext {
 			locals = LocalCapture.CAPTURE_FAILEXCEPTION, remap = false)
 	private void isVecInRangeWisp (Vec3 vec, CallbackInfoReturnable<Boolean> cir) {
 		if (this.wisp != null) {
-			cir.setReturnValue(vec.distanceToSqr(this.wisp.position()) < this.wisp.maxSqrCastingDistance());
+			cir.setReturnValue(vec.distanceToSqr(this.wisp.position()) <= this.wisp.maxSqrCastingDistance());
 		}
 	}
 }
