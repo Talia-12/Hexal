29afe66675b621538d811497d4c3a25cc8bcc236
Talia-12 <paradox.m.burgess@gmail.com>
2023-03-23 19:06:05 +1000

    Standardise OpGetStorageRemainingCapacity with other similar actions.

--- Common/src/main/java/ram/talia/hexal/common/casting/actions/spells/items/OpGetStorageRemainingCapacity.kt
+++ Common/src/main/java/ram/talia/hexal/common/casting/actions/spells/items/OpGetStorageRemainingCapacity.kt
@@ -6,27 +6,17 @@ import at.petrak.hexcasting.api.spell.ConstMediaAction
 import at.petrak.hexcasting.api.spell.asActionResult
 import at.petrak.hexcasting.api.spell.casting.CastingContext
 import at.petrak.hexcasting.api.spell.iota.Iota
-import at.petrak.hexcasting.api.spell.mishaps.MishapInvalidIota
 import ram.talia.hexal.api.config.HexalConfig
-import ram.talia.hexal.api.getBlockPosOrNull
 import ram.talia.hexal.api.mediafieditems.MediafiedItemManager
 import ram.talia.hexal.api.spell.casting.IMixinCastingContext
 import ram.talia.hexal.api.spell.mishaps.MishapNoBoundStorage
-import ram.talia.hexal.common.blocks.entity.BlockEntityMediafiedStorage
 
 object OpGetStorageRemainingCapacity : ConstMediaAction {
-    override val argc = 1
+    override val argc = 0
 
     override fun execute(args: List<Iota>, ctx: CastingContext): List<Iota> {
-        val pos = args.getBlockPosOrNull(0, argc)
 
-        pos?.let { ctx.assertVecInRange(it) }
-
-        val storage = if (pos == null) {
-            (ctx as IMixinCastingContext).boundStorage?.let { MediafiedItemManager.getStorage(it)?.get() } ?: throw MishapNoBoundStorage(ctx.caster.position())
-        }  else {
-            ctx.world.getBlockEntity(pos) as? BlockEntityMediafiedStorage ?: throw MishapInvalidIota.ofType(args[1], 0, "mediafied_storage")
-        }
+        val storage = (ctx as IMixinCastingContext).boundStorage?.let { MediafiedItemManager.getStorage(it)?.get() } ?: throw MishapNoBoundStorage(ctx.caster.position())
 
         return (HexalConfig.server.maxRecordsInMediafiedStorage - storage.storedItems.size).asActionResult
     }
