f796bd80af21839cd55bac4a8ce320b0b61db300
Talia-12 <paradox.m.burgess@gmail.com>
2023-03-24 15:58:09 +1000

    index errors again with OpGetMarkedGate

--- Common/src/main/java/ram/talia/hexal/common/casting/actions/spells/gates/OpGetMarkedGate.kt
+++ Common/src/main/java/ram/talia/hexal/common/casting/actions/spells/gates/OpGetMarkedGate.kt
@@ -4,7 +4,6 @@ import at.petrak.hexcasting.api.spell.ConstMediaAction
 import at.petrak.hexcasting.api.spell.asActionResult
 import at.petrak.hexcasting.api.spell.casting.CastingContext
 import at.petrak.hexcasting.api.spell.getEntity
-import at.petrak.hexcasting.api.spell.iota.EntityIota
 import at.petrak.hexcasting.api.spell.iota.Iota
 import ram.talia.hexal.api.getGate
 
@@ -13,7 +12,7 @@ object OpGetMarkedGate : ConstMediaAction {
 
     override fun execute(args: List<Iota>, ctx: CastingContext): List<Iota> {
         val gate = args.getGate(0, argc)
-        val entity = args.getEntity(0, argc)
+        val entity = args.getEntity(1, argc)
         return gate.isMarked(entity).asActionResult
     }
 }
