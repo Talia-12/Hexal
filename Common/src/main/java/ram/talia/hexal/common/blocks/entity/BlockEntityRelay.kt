f18d57cefa3e7ad73e6f46d4a2ac4f8f993049fc
Talia-12 <paradox.m.burgess@gmail.com>
2023-05-29 21:32:14 +1000

    rewrote LazyILinkableList pretty extensively, now if A and B are linked and unloaded together, then A is loaded without B, then B is loaded, they should relink.

--- Common/src/main/java/ram/talia/hexal/common/blocks/entity/BlockEntityRelay.kt
+++ Common/src/main/java/ram/talia/hexal/common/blocks/entity/BlockEntityRelay.kt
@@ -4,10 +4,15 @@ import at.petrak.hexcasting.api.block.HexBlockEntity
 import at.petrak.hexcasting.api.misc.FrozenColorizer
 import at.petrak.hexcasting.api.spell.asActionResult
 import at.petrak.hexcasting.api.spell.iota.Iota
+import at.petrak.hexcasting.api.utils.getList
+import at.petrak.hexcasting.api.utils.putCompound
+import at.petrak.hexcasting.api.utils.putList
 import at.petrak.hexcasting.common.lib.HexItems
 import net.minecraft.Util
 import net.minecraft.core.BlockPos
 import net.minecraft.nbt.CompoundTag
+import net.minecraft.nbt.IntTag
+import net.minecraft.nbt.ListTag
 import net.minecraft.server.level.ServerLevel
 import net.minecraft.util.RandomSource
 import net.minecraft.world.item.DyeColor
@@ -27,7 +32,7 @@ class BlockEntityRelay(pos: BlockPos, val state: BlockState) : HexBlockEntity(He
     private val random = RandomSource.create()
 
     private var relayNetwork: MutableSet<BlockEntityRelay> = mutableSetOf(this)
-    private var directlyLinkedRelays: MutableSet<BlockEntityRelay> = mutableSetOf()
+    private var directlyLinkedRelays: MutableSet<SerialisedBlockEntityRelay> = mutableSetOf()
     private var numNonRelaysLinked = 0
     private var numMediaAcceptorsLinked = 0
     private var lastTickComputedAverageMedia = 0L
@@ -79,11 +84,11 @@ class BlockEntityRelay(pos: BlockPos, val state: BlockState) : HexBlockEntity(He
             this.relayNetwork.forEach { it.setColouriser(other.colouriser, level!!) }
 
         this.relayNetwork.addAll(other.relayNetwork)
-        this.directlyLinkedRelays.add(other)
+        this.directlyLinkedRelays.add(other.toSerWrap())
         this.nonRelaysLinked.addAll(other.nonRelaysLinked)
         this.mediaExchangersLinked.addAll(other.mediaExchangersLinked)
         other.relayNetwork = this.relayNetwork
-        other.directlyLinkedRelays.add(this)
+        other.directlyLinkedRelays.add(this.toSerWrap())
         other.nonRelaysLinked = this.nonRelaysLinked
         other.mediaExchangersLinked = this.mediaExchangersLinked
     }
@@ -97,7 +102,8 @@ class BlockEntityRelay(pos: BlockPos, val state: BlockState) : HexBlockEntity(He
         unvisited.remove(this)
 
         val newNetwork = mutableSetOf(this)
-        val frontier = this.directlyLinkedRelays.toMutableList()
+        // only add relays that are currently loaded to the frontier
+        val frontier = this.directlyLinkedRelays.mapNotNull { this.level?.let { level -> it.getRelay(level) } }.toMutableList()
 
         while (frontier.isNotEmpty()) {
             val next = frontier.removeFirst()
@@ -107,9 +113,11 @@ class BlockEntityRelay(pos: BlockPos, val state: BlockState) : HexBlockEntity(He
                 return
             newNetwork.add(next)
             unvisited.remove(next)
-            frontier.addAll(next.directlyLinkedRelays.filter { it !in newNetwork })
+            // only add relays that are currently loaded to the frontier
+            frontier.addAll(next.directlyLinkedRelays.mapNotNull { this.level?.let { level -> it.getRelay(level) } }.filter { it !in newNetwork })
         }
 
+        // separate out the non-relays that are connected to this's new network vs other's new network
         val (newNonRelaysLinked, newMediaExchangersLinked) = newNetwork.fold(mutableSetOf<ILinkable>() to mutableSetOf<ILinkable>())
             { (nonRelays, mediaExchangers), relay ->
                 nonRelays.addAll(relay.nonRelaysLinkedDirectly); mediaExchangers.addAll(relay.mediaExchangersLinkedDirectly)
@@ -121,6 +129,7 @@ class BlockEntityRelay(pos: BlockPos, val state: BlockState) : HexBlockEntity(He
             nonRelays to mediaExchangers
         }
 
+        // assign everything in the new network to contain the correct sets of things in and adjacent to the network. Do likewise for other's new network.
         newNetwork.forEach { it.relayNetwork = newNetwork; it.nonRelaysLinked = newNonRelaysLinked; it.mediaExchangersLinked = newMediaExchangersLinked }
         unvisited.forEach { it.relayNetwork = unvisited; it.nonRelaysLinked = unvisitedNonRelaysLinked; it.mediaExchangersLinked = unvisitedMediaExchangersLinked }
     }
@@ -131,10 +140,12 @@ class BlockEntityRelay(pos: BlockPos, val state: BlockState) : HexBlockEntity(He
         get() = pos.asActionResult
 
     private var cachedLinkableHolder: ServerLinkableHolder? = null
+    private var serialisedLinkableHolder: CompoundTag? = null
 
     override val linkableHolder: ServerLinkableHolder?
         get() = cachedLinkableHolder ?: let {
                 cachedLinkableHolder = (this.level as? ServerLevel)?.let { ServerLinkableHolder(this, it) }
+                serialisedLinkableHolder?.let { cachedLinkableHolder?.readFromNbt(it)?.let { serialisedLinkableHolder = null } }
                 cachedLinkableHolder
             }
 
@@ -201,8 +212,8 @@ class BlockEntityRelay(pos: BlockPos, val state: BlockState) : HexBlockEntity(He
             // don't need to run this on both relays
             if (!unlinkOther)
                 return
-            this.directlyLinkedRelays.remove(other)
-            other.directlyLinkedRelays.remove(this)
+            this.directlyLinkedRelays.remove(other.toSerWrap())
+            other.directlyLinkedRelays.remove(this.toSerWrap())
             // uncombine the networks of other and this.
             findSeparateNetworks(other)
         } else {
@@ -244,15 +255,69 @@ class BlockEntityRelay(pos: BlockPos, val state: BlockState) : HexBlockEntity(He
     override fun loadModData(tag: CompoundTag) {
         if (tag.contains(TAG_COLOURISER))
             colouriser = FrozenColorizer.fromNBT(tag.getCompound(TAG_COLOURISER))
+        if (tag.contains(TAG_COLOURISER_TIME))
+            timeColouriserSet = tag.getLong(TAG_COLOURISER_TIME)
+        if (tag.contains(TAG_LINKABLE_HOLDER))
+            serialisedLinkableHolder = tag.getCompound(TAG_LINKABLE_HOLDER)
+        if (tag.contains(TAG_DIRECTLY_LINKED_RELAYS))
+            directlyLinkedRelaysFromTag(tag.getList(TAG_DIRECTLY_LINKED_RELAYS, ListTag.TAG_LIST))
     }
 
     override fun saveModData(tag: CompoundTag) {
         tag.put(TAG_COLOURISER, colouriser.serializeToNBT())
+        tag.putLong(TAG_COLOURISER_TIME, timeColouriserSet)
+        tag.putCompound(TAG_LINKABLE_HOLDER, linkableHolder!!.writeToNbt())
+        tag.putList(TAG_DIRECTLY_LINKED_RELAYS, directlyLinkedRelaysToTag())
+    }
+
+    private fun directlyLinkedRelaysToTag(): ListTag {
+        val out = ListTag()
+        directlyLinkedRelays.forEach { out.add(blockPosToListTag(it.pos)) }
+        return out
+    }
+
+    private fun directlyLinkedRelaysFromTag(tag: ListTag) {
+        tag.forEach {
+            directlyLinkedRelays.add(SerialisedBlockEntityRelay(listTagToBlockPos(it as ListTag)))
+        }
+    }
+
+    private fun toSerWrap(): SerialisedBlockEntityRelay = SerialisedBlockEntityRelay(this)
+
+    private data class SerialisedBlockEntityRelay(val pos: BlockPos) {
+        constructor(relay: BlockEntityRelay) : this(relay.pos) {
+            this.relay = relay
+        }
+
+        private var relay: BlockEntityRelay? = null
+
+        fun getRelay(level: Level?): BlockEntityRelay? = relay ?: let {
+            relay = level?.getBlockEntity(pos) as? BlockEntityRelay
+            relay
+        }
     }
 
     companion object {
         const val MAX_SQR_LINK_RANGE = 32.0*32.0
 
         const val TAG_COLOURISER = "hexal:colouriser"
+        const val TAG_COLOURISER_TIME = "hexal:colouriser_time"
+        const val TAG_LINKABLE_HOLDER = "hexal:linkable_holder"
+        const val TAG_DIRECTLY_LINKED_RELAYS = "hexal:directly_linked_relays"
+
+        private fun blockPosToListTag(pos: BlockPos): ListTag {
+            val listTag = ListTag()
+            listTag.add(IntTag.valueOf(pos.x))
+            listTag.add(IntTag.valueOf(pos.y))
+            listTag.add(IntTag.valueOf(pos.z))
+            return listTag
+        }
+
+        private fun listTagToBlockPos(listTag: ListTag): BlockPos {
+            val x = listTag.getInt(0)
+            val y = listTag.getInt(1)
+            val z = listTag.getInt(2)
+            return BlockPos(x, y, z)
+        }
     }
 }
