a89b1a5581c58f8cd1a4679a117e11ac2732ef0b
Talia-12 <paradox.m.burgess@gmail.com>
2023-05-31 00:03:19 +1000

    direction stuff all working properly! syncing colour when networks combine seems to be broken

--- Common/src/main/java/ram/talia/hexal/common/blocks/entity/BlockEntityRelay.kt
+++ Common/src/main/java/ram/talia/hexal/common/blocks/entity/BlockEntityRelay.kt
@@ -26,6 +26,8 @@ import ram.talia.hexal.api.config.HexalConfig
 import ram.talia.hexal.api.linkable.*
 import ram.talia.hexal.api.linkable.ILinkable.LazyILinkableSet
 import ram.talia.hexal.api.plus
+import ram.talia.hexal.api.times
+import ram.talia.hexal.common.blocks.BlockRelay
 import ram.talia.hexal.common.lib.HexalBlockEntities
 import software.bernie.geckolib3.core.IAnimatable
 import software.bernie.geckolib3.core.PlayState
@@ -272,7 +274,7 @@ class BlockEntityRelay(pos: BlockPos, val state: BlockState) : HexBlockEntity(He
     private fun getBobberPosition(): Vec3 {
         val manager = factory.getOrCreateAnimationData(this.pos.hashCode()) // this is how the unique ID is calculated in GeoBlockRenderer
         val bobber = manager.boneSnapshotCollection["Bobber"]?.right ?: return Vec3.ZERO
-        return Vec3(bobber.positionOffsetX / 16.0, (bobber.positionOffsetY + 10) / 16.0, bobber.positionOffsetZ / 16.0)
+        return (bobber.positionOffsetY + 10) / 16.0 * Vec3.atLowerCornerOf(state.getValue(BlockRelay.FACING).normal)
     }
     //endregion
 
