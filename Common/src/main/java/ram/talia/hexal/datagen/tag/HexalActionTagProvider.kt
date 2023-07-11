906b28434c09615a21c99ee7e46ad2483b4d8e0b
Talia-12 <paradox.m.burgess@gmail.com>
2023-07-11 22:28:01 +1000

    fix action tag provider

--- Common/src/main/java/ram/talia/hexal/datagen/tag/HexalActionTagProvider.kt
+++ Common/src/main/java/ram/talia/hexal/datagen/tag/HexalActionTagProvider.kt
@@ -1,6 +1,5 @@
 package ram.talia.hexal.datagen.tag
 
-import at.petrak.hexcasting.api.HexAPI
 import at.petrak.hexcasting.api.casting.ActionRegistryEntry
 import at.petrak.hexcasting.api.mod.HexTags
 import at.petrak.hexcasting.xplat.IXplatAbstractions
@@ -8,6 +7,7 @@ import net.minecraft.core.HolderLookup
 import net.minecraft.data.PackOutput
 import net.minecraft.data.tags.TagsProvider
 import net.minecraft.resources.ResourceKey
+import ram.talia.hexal.api.HexalAPI.modLoc
 import java.util.concurrent.CompletableFuture
 
 class HexalActionTagProvider(output: PackOutput, provider: CompletableFuture<HolderLookup.Provider>)
@@ -18,7 +18,7 @@ class HexalActionTagProvider(output: PackOutput, provider: CompletableFuture<Hol
         for (normalGreat in arrayOf(
                 "wisp/consume", "wisp/seon/set", "tick", "gate/make"
         )) {
-            val loc = HexAPI.modLoc(normalGreat)
+            val loc = modLoc(normalGreat)
             val key = ResourceKey.create(IXplatAbstractions.INSTANCE.actionRegistry.key(), loc)
             tag(HexTags.Actions.REQUIRES_ENLIGHTENMENT).add(key)
             tag(HexTags.Actions.CAN_START_ENLIGHTEN).add(key)
