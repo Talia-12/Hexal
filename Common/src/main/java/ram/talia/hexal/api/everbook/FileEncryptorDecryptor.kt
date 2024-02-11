6360d6b64197270b201822747cc1e13182db2011
Talia-12 <paradox.m.burgess@gmail.com>
2024-02-11 23:27:11 +1000

    version inc, fix file.parentFile maybe returning null

--- Common/src/main/java/ram/talia/hexal/api/everbook/FileEncryptorDecryptor.kt
+++ Common/src/main/java/ram/talia/hexal/api/everbook/FileEncryptorDecryptor.kt
@@ -26,7 +26,7 @@ internal class FileEncrypterDecrypter(private val secretKey: SecretKey, cipher: 
 	fun encrypt(content: String, file: File) {
 		cipher.init(Cipher.ENCRYPT_MODE, secretKey)
 		val iv = cipher.iv
-		file.parentFile.mkdirs()
+		file.absoluteFile.parentFile.mkdirs()
 		file.createNewFile()
 		FileOutputStream(file).use { fileOut ->
 			CipherOutputStream(fileOut, cipher).use { cipherOut ->
@@ -40,7 +40,7 @@ internal class FileEncrypterDecrypter(private val secretKey: SecretKey, cipher: 
 	fun encrypt(content: CompoundTag, file: File) {
 		cipher.init(Cipher.ENCRYPT_MODE, secretKey)
 		val iv = cipher.iv
-		file.parentFile.mkdirs()
+		file.absoluteFile.parentFile.mkdirs()
 		file.createNewFile()
 		FileOutputStream(file).use { fileOut ->
 			fileOut.write(iv)
